5201316e4cd8d997cd4090fe16be393cc75d18fb
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-08-29 15:58:40 +0530

    init

---
+++ django-sandbox/my_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+
+    return HttpResponse("Hello World")
+
+
+def about(request):
+
+    return HttpResponse("Hi, my name is Sid!")
